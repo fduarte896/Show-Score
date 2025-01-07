127d3ea7d05cd61f884bb73128fb0d3f0af6183c
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-01-07 14:12:48 -0500

    Initial Commit

---
+++ Show Score/Show_ScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Show_ScoreApp.swift
+//  Show Score
+//
+//  Created by Felipe Duarte on 7/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Show_ScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
