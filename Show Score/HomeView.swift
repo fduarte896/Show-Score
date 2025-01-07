e0ecb7713952b8a5ca6703f582bb2bb7687033a7
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-01-07 14:19:53 -0500

    First view: homeview with placeholder

---
+++ Show Score/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  Show Score
+//
+//  Created by Felipe Duarte on 7/01/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(0..<10) {
+                    Text("Item \( $0 )")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
