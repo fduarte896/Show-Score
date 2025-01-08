b46937ed7b4177ad2ab4bfe6dd6bf9f5355edb1b
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-01-08 16:25:47 -0500

    Creating the first models and structs to properly handle the API response for the popular movies.

---
+++ Show Score/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Show Score
+//
+//  Created by Felipe Duarte on 8/01/25.
+//
+
+import Foundation
---
+++ Show Score/PopularJSONResponseExample.swift
@@ -0,0 +1,8 @@
+//
+//  PopularJSONResponseExample.swift
+//  Show Score
+//
+//  Created by Felipe Duarte on 8/01/25.
+//
+
+import Foundation
