6ff1b732703eb72b81a2cf865989082cebfc6166
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-01-20 11:02:29 -0500

    Hasta aquí hay una forma funcional (aunque no sé qué tan probada o escalable) de la app. Intentaré hacer una alternativa donde pongo el modelContext desde el App.

---
+++ Show Score/Interactor/URLSession.swift
@@ -0,0 +1,8 @@
+//
+//  URLSession.swift
+//  Show Score
+//
+//  Created by Felipe Duarte on 15/01/25.
+//
+
+import Foundation
---
+++ Show Score/Interfaces/URLRequest.swift
@@ -0,0 +1,8 @@
+//
+//  URLRequest.swift
+//  Show Score
+//
+//  Created by Felipe Duarte on 15/01/25.
+//
+
+import Foundation
