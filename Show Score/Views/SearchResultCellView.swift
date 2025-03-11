1422b9453b16e890beb576efa6707a41c9167d9c
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-03-11 12:33:23 -0500

    The search bar is working and showing movies, tv shows and people properly. The details for movies is working, but the next step is to do the same with TV Shows and then start with the visuals.

---
+++ Show Score/Views/SearchResultCellView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultCellView.swift
+//  Show Score
+//
+//  Created by Felipe Duarte on 6/03/25.
+//
+
+import SwiftUI
+
+struct SearchResultCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResultCellView()
+}
