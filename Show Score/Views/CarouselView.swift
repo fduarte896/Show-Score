239093cba1a58d9d69eb2757a49aa7e7d8e97882
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-03-18 10:27:35 -0500

    HomeView redesigned, however it still need optimization with the scroll. Next step: redesign the favs tabs, implement deleting favs.

---
+++ Show Score/Views/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  Show Score
+//
+//  Created by Felipe Duarte on 12/03/25.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
